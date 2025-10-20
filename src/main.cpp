a4a1921fc94f68f86d786b9072d199af7791ed1f
Danila <deny.ganshin120@gmail.com>
2025-10-20 15:40:08 +0300

    закончил

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 const int CAR_RED = 23;
 const int CAR_YELLOW = 22;
 const int CAR_GREEN = 21;
-const int PED_RED =   17;
-const int PED_GREEN = 16;
 const int BUTTON =    13;
 
+const int red_time = 5000;
+const int green_time = 3000;
+const int blinking_time = 500;
+const int AutoModeDuration = red_time + green_time + 4*(blinking_time * 4); 
 
-
+unsigned long currentTime = millis();
 
 void setup()
 {
@@ -17,46 +19,36 @@ void setup()
   pinMode(CAR_RED, OUTPUT);
   pinMode(CAR_YELLOW, OUTPUT);
   pinMode(CAR_GREEN, OUTPUT);
-  pinMode(PED_RED, OUTPUT);
-  pinMode(PED_GREEN, OUTPUT);
 
   digitalWrite(CAR_RED, HIGH);
   digitalWrite(CAR_YELLOW, LOW);
   digitalWrite(CAR_GREEN, LOW);
+
+  Serial.println(AutoModeDuration);
 }
 
 void loop()
 {
   digitalWrite(CAR_RED, HIGH);
-  delay(2000);                     // машины стоят, пешеходы идут
-  digitalWrite(PED_RED, LOW);
-  digitalWrite(PED_GREEN, HIGH);
-  delay(4000);
-  for(int i = 0; i <= 3; i++){     // мигает зелёный сигнал пешеходам
-    delay(500);
-    digitalWrite(PED_GREEN, HIGH);
-    delay(500);
-    digitalWrite(PED_GREEN, LOW);
-  }
-  digitalWrite(PED_RED, HIGH);
-  delay(2000);                     // пешеходы встали
-
+  delay(red_time);
   digitalWrite(CAR_RED, LOW);
 
   digitalWrite(CAR_YELLOW, HIGH);  // жёлтый сигнал
-  delay(1000);
+  delay(blinking_time * 4);
   digitalWrite(CAR_YELLOW, LOW);
 
-  digitalWrite(CAR_GREEN, HIGH);   // машины едут, пешеходы стоят
-  delay(4000);
+  digitalWrite(CAR_GREEN, HIGH);   // машины едут
+  delay(green_time);
   for(int i = 0; i <= 3; i++){     // мигает зелёный сигнал машинам
-    delay(500);
     digitalWrite(CAR_GREEN, HIGH);
-    delay(500);
+    delay(blinking_time);
     digitalWrite(CAR_GREEN, LOW);
+    delay(blinking_time);
   }
 
   digitalWrite(CAR_YELLOW, HIGH);  // жёлтый сигнал
-  delay(1000);
+  delay(blinking_time * 4);
   digitalWrite(CAR_YELLOW, LOW);
+
+  Serial.println(currentTime);
 }
