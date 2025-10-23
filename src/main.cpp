92187ac5cddb8ff4701cf73e25fd00934926c015
DANILA GANSHIN <125364917+Cooper-Farnsworth@users.noreply.github.com>
2025-10-23 14:35:27 +0300

    трафик без кнопки

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@ const int CAR_RED = 23;
 const int CAR_YELLOW = 22;
 const int CAR_GREEN = 21;
 const int BUTTON =    13;
+bool cars_mode = true;
 
-const int red_time = 5000;
-const int green_time = 3000;
-const int blinking_time = 500;
-const int AutoModeDuration = red_time + green_time + 4*(blinking_time * 4); 
-
-unsigned long currentTime = millis();
+uint32_t current_time = 0;
+uint32_t previous_time = 0;
+uint32_t red_time = 5000;
+uint32_t green_time = 3000;
+uint32_t blinking_time = 500;
+uint32_t AutoModeDuration = red_time + green_time + blinking_time*4; 
 
 void setup()
 {
@@ -19,36 +20,51 @@ void setup()
   pinMode(CAR_RED, OUTPUT);
   pinMode(CAR_YELLOW, OUTPUT);
   pinMode(CAR_GREEN, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
 
-  digitalWrite(CAR_RED, HIGH);
+  digitalWrite(CAR_RED, LOW);
   digitalWrite(CAR_YELLOW, LOW);
-  digitalWrite(CAR_GREEN, LOW);
+  digitalWrite(CAR_GREEN, HIGH);
 
   Serial.println(AutoModeDuration);
 }
 
-void loop()
-{
-  digitalWrite(CAR_RED, HIGH);
-  delay(red_time);
+void delay_func(uint32_t time) {
+  uint32_t prev = millis();
+  while(millis() - prev <= time);
+}
+
+bool handle_button() {
+  if(digitalRead(BUTTON)) return true;
+  return false;
+}
+
+void auto_traffic_mode() {
+  digitalWrite(CAR_GREEN, HIGH);   // машины едут
+  delay_func(green_time);
+  digitalWrite(CAR_GREEN, LOW);
+
+  digitalWrite(CAR_YELLOW, HIGH);  // жёлтый сигнал
+  delay_func(blinking_time * 2);
+  digitalWrite(CAR_YELLOW, LOW);
+
+  digitalWrite(CAR_RED, HIGH); // машины встали
+  delay_func(red_time);
   digitalWrite(CAR_RED, LOW);
 
   digitalWrite(CAR_YELLOW, HIGH);  // жёлтый сигнал
-  delay(blinking_time * 4);
+  delay_func(blinking_time * 2);
   digitalWrite(CAR_YELLOW, LOW);
+}
 
-  digitalWrite(CAR_GREEN, HIGH);   // машины едут
-  delay(green_time);
-  for(int i = 0; i <= 3; i++){     // мигает зелёный сигнал машинам
-    digitalWrite(CAR_GREEN, HIGH);
-    delay(blinking_time);
-    digitalWrite(CAR_GREEN, LOW);
-    delay(blinking_time);
+void manual_traffic_mode() {
+}
+
+void loop()
+{ 
+  current_time = millis();
+  if(cars_mode && (current_time - previous_time >= AutoModeDuration)) {
+    previous_time = current_time;
+    auto_traffic_mode();
   }
-
-  digitalWrite(CAR_YELLOW, HIGH);  // жёлтый сигнал
-  delay(blinking_time * 4);
-  digitalWrite(CAR_YELLOW, LOW);
-
-  Serial.println(currentTime);
 }
